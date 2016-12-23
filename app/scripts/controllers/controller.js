b68f77d791cb2c9e5a7258bf2428b47c9b4e94e1
Jérôme Moret <jermoret@gmail.com>
2016-12-23 14:19:01 +0100

    Finish last implementations (equivalent household reduction and sources provenances)

--- app/scripts/controllers/controller.js
+++ app/scripts/controllers/controller.js
@@ -134,22 +134,19 @@ angular.module('swissNuclearExitApp')
     }
 
     function refreshSummaryGraph() {
-      $scope.summary_data = [
-        [$scope.wind_strategy_cover_percentage],
-        [$scope.solar_strategy_cover_percentage],
-        [$scope.reduction_strategy_cover_percentage],
-        [$scope.imports_strategy_cover_percentage]
-      ];
+      $scope.summary_data = [];
+      for(var i = 0; i < $scope.strategy.length; i++) {
+        $scope.summary_data.push([$scope.strategy[i].percentage]);
+      }
       $scope.summary_options.scales.xAxes[0].ticks.max = 100;
+      $scope.summary_options.scales.xAxes[0].scaleLabel = {
+        display: true,
+        labelString: 'of ' + $scope.nuclearDeficiencyRatio * -1 + ' ' + GWH_UNITY
+      };
     }
 
-
-    $scope.$on('create', function (event, chart) {
-      //document.getElementById('pie_chart_legend').innerHTML = chart.generateLegend();
-    });
-
+    // Get data from the JSON file
     $http.get('../data/graphs_data.json').then(function(response) {
-      //$scope.data = response.data;
       $scope.centrals = response.data.swiss_nuclear_centrals;
       $scope.euro_stats_2014 = response.data.european_statistics_2014;
       $scope.swiss_stats_2014 = $scope.euro_stats_2014[0];
@@ -160,7 +157,7 @@ angular.module('swissNuclearExitApp')
       $scope.swiss_wind_2014 = $scope.renew_pie_data[5];
       $scope.swiss_solar_2014 = $scope.renew_pie_data[3];
       $scope.swiss_total_production = $scope.renew_pie_data.reduce((pv, cv) => pv+cv, 0);
-
+      $scope.swiss_per_house_cons_2014 = $scope.swiss_stats_2014.comsumption.comsumption_by_households_kWh;
       $scope.renew_bar_labels = [];
       $scope.renew_bar_data = [];
 
@@ -206,6 +203,8 @@ angular.module('swissNuclearExitApp')
       $scope.solar_households_supply_unity = response.data.renewables_energies.solar.household_supply_unity;
 
       $scope.soccer_fields_size_m2 = response.data.equivalent.soccer_field_m2;
+      $scope.led_bulb_power = response.data.bulbs[0].power;
+      $scope.halo_bulb_power = response.data.bulbs[1].power;
     });
 
     $scope.default_colors = ['#F7464A','#8C4906','#337AB7','#FCD552','#85CA3A','#ADC9D7','#303030'];
@@ -215,25 +214,33 @@ angular.module('swissNuclearExitApp')
     $scope.generalDeficiencyRatio = 0;
     $scope.nuclearDeficiencyRatio = 0;
     $scope.badge_color = "label-danger";
-    $scope.wind_strategy_cover_percentage = 0;
-    $scope.wind_strategy_cover = 0;
-    $scope.solar_strategy_cover_percentage = 0;
-    $scope.solar_strategy_cover = 0;
-    $scope.reduction_strategy_cover_percentage = 0;
-    $scope.reduction_strategy_cover = 0;
-    $scope.imports_strategy_cover_percentage = 0;
-    $scope.imports_strategy_cover = 0;
+    $scope.strategy = [
+      {
+        cover:0,
+        percentage:0
+      },
+      {
+        cover:0,
+        percentage:0
+      },
+      {
+        cover:0,
+        percentage:0
+      },
+      {
+        cover:0,
+        percentage:0
+      },
+    ];
     $scope.wind_input = 0;
     $scope.solar_input = 0;
     $scope.wind_households_supply = 0;
     $scope.solar_households_supply = 0;
     $scope.equivalent_soccer_fields = 0;
     $scope.equivalent_beznau_1 = 0;
+    $scope.equivalent_bulbs_replacement = 0;
     $scope.summary_series = ["Wind strategy","Solar strategy","Households consumption reduction","Imports strategy"];
-    $scope.reduction = {
-      options: [0,100,150,200,250],
-      value: 0
-    };
+    $scope.reduction_value = 0;
     $scope.imports_value = 0;
 
     // Initialize toggles
@@ -261,11 +268,11 @@ angular.module('swissNuclearExitApp')
       var solar_cover = $scope.solar_input * $scope.solar_anual_production_unit;
       $scope.generalDeficiencyRatio -= $scope.old_renew_value;
       $scope.generalDeficiencyRatio += wind_cover + solar_cover;
-      $scope.wind_strategy_cover = wind_cover;
-      $scope.solar_strategy_cover = solar_cover;
 
-      $scope.wind_strategy_cover_percentage = computeCoverPercentage(wind_cover);
-      $scope.solar_strategy_cover_percentage = computeCoverPercentage(solar_cover);
+      $scope.strategy[0].cover = wind_cover;
+      $scope.strategy[1].cover = solar_cover;
+      $scope.strategy[0].percentage = computeCoverPercentage(wind_cover);
+      $scope.strategy[1].percentage = computeCoverPercentage(solar_cover);
 
       $scope.wind_households_supply = $scope.wind_input * $scope.wind_households_supply_unity;
       $scope.solar_households_supply = $scope.solar_input * $scope.solar_households_supply_unity;
@@ -282,18 +289,26 @@ angular.module('swissNuclearExitApp')
 
     $scope.old_reduction_value = 0;
     $scope.old_reduction_kwh = 0;
-    $scope.selectChange = function () {
+
+    $scope.reductionInputChange = function () {
       $scope.generalDeficiencyRatio -= $scope.old_reduction_value;
-      var reduction_kwh = $scope.reduction.value;
-      var reduction = NB_SWISS_HOUSEHOLDS * $scope.reduction.value / 1000000;
+      var reduction_kwh = $scope.reduction_value / 100 * $scope.swiss_per_house_cons_2014;
+      var reduction = NB_SWISS_HOUSEHOLDS * reduction_kwh / 1000000;
       $scope.generalDeficiencyRatio += reduction;
-      $scope.reduction_strategy_cover = reduction;
-      $scope.reduction_strategy_cover_percentage = computeCoverPercentage(reduction);
+
+      $scope.strategy[2].cover = reduction;
+      $scope.strategy[2].percentage = computeCoverPercentage(reduction);
+
       var length = $scope.total_household_cons_data[0].length;
       $scope.total_household_cons_data[0][length-1] += $scope.old_reduction_value;
       $scope.total_household_cons_data[0][length-1] -= reduction;
       $scope.per_house_cons_data[0][0] += $scope.old_reduction_kwh;
       $scope.per_house_cons_data[0][0] -= reduction_kwh;
+
+      var halo_year_wh = $scope.halo_bulb_power * 4 * 365;
+      var led_year_wh = $scope.led_bulb_power * 4 * 365;
+      var difference_kwh = (halo_year_wh - led_year_wh) / 1000;
+      $scope.equivalent_bulbs_replacement = reduction_kwh / difference_kwh;
 
       $scope.old_reduction_value = reduction;
       $scope.old_reduction_kwh = reduction_kwh;
@@ -306,8 +321,9 @@ angular.module('swissNuclearExitApp')
       $scope.generalDeficiencyRatio -= $scope.old_import_value;
       var imports = $scope.imports_value;
       $scope.generalDeficiencyRatio += imports;
-      $scope.imports_strategy_cover = imports;
-      $scope.imports_strategy_cover_percentage = computeCoverPercentage(imports);
+
+      $scope.strategy[3].cover = imports;
+      $scope.strategy[3].percentage = computeCoverPercentage(imports);
 
       $scope.swiss_imp_exp_data[0][0] -= $scope.old_import_value / 2;
       $scope.swiss_imp_exp_data[0][1] -= $scope.old_import_value / 2;
@@ -390,7 +406,11 @@ angular.module('swissNuclearExitApp')
           stacked: true,
           ticks: {
             callback: function(value, index, values) {
-              return value + ' %';
+              var percentage = value + ' %'
+              /*if(value === 100) {
+                percentage += '\n (' + $scope.nuclearDeficiencyRatio * -1 + ' GWh)';
+              }*/
+              return percentage;
             }
           }
         }],
@@ -401,7 +421,8 @@ angular.module('swissNuclearExitApp')
       tooltips: {
         callbacks: {
           label: function(tooltipItem, data) {
-            return data.datasets[tooltipItem.datasetIndex].label + ": " + tooltipItem.xLabel.toFixed(1) + ' %';
+            return data.datasets[tooltipItem.datasetIndex].label + ": " + tooltipItem.xLabel.toFixed(1) + ' % (' +
+              $scope.strategy[tooltipItem.datasetIndex].cover.toFixed(1) + ' ' + GWH_UNITY + ')';
           }
         }
       }
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -23,7 +23,6 @@ module.exports = function(config) {
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
-      'bower_components/angular-google-chart/ng-google-chart.js',
       'bower_components/angular-bootstrap-toggle/dist/angular-bootstrap-toggle.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
